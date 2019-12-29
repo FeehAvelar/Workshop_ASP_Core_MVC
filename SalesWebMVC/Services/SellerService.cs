1ed710064f1974f6e93a287d7d1f6ad676d96481
Felipe Avelar <felipealpaca@gmail.com>
2019-12-29 15:05:09 -0300

    Simple Create form

--- SalesWebMVC/Services/SellerService.cs
+++ SalesWebMVC/Services/SellerService.cs
@@ -20,5 +20,10 @@ namespace SalesWebMVC.Services
             return _context.Seller.ToList();
         }
 
+        public void Insert(Seller seller)
+        {
+            _context.Seller.Add(seller);
+            _context.SaveChanges(); 
+        }
     }
 }
